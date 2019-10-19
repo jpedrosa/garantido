8cdf875c81625a5b4bd0b1b9e1e8eaa4740436d7
Joao Pedrosa <joaopedrosa@gmail.com>
2019-10-19 06:07:42 -0300

    Added AliasOpts.

---
+++ fs/opts/aliasopts/src/aliasopts.rs
@@ -0,0 +1,129 @@
+// aliasopts.rs
+// Copyright (c) 2019 Joao Pedrosa
+
+use std::collections::HashMap;
+
+
+#[derive(Debug, PartialEq)]
+pub enum CT {
+    CTString,
+    Flag,
+    Alias
+}
+
+#[derive(Debug)]
+pub struct AliasOpts {
+    patterns: HashMap<String, (CT, String)>,
+    values: HashMap<String, String>,
+    loose: Vec<String>,
+}
+
+impl AliasOpts {
+
+    pub fn new() -> AliasOpts {
+        AliasOpts {
+            patterns: HashMap::new(),
+            values: HashMap::new(),
+            loose: Vec::new()
+        } 
+    }
+
+    pub fn add(& mut self, s: &str, ct: CT) -> & mut AliasOpts {
+        self.patterns.insert(s.to_string(), (ct, String::new()));
+        self
+    }
+
+    pub fn add_alias(&mut self, alias: &str, alias_for: &str) -> 
+        &mut AliasOpts {
+        if let Some(v) = self.patterns.get(alias_for) {
+            if v.0 != CT::Alias {
+                self.patterns.insert(alias.to_string(),
+                    (CT::Alias, alias_for.to_string()));
+            }
+        }
+        self
+    }
+
+    pub fn parse(&mut self, args: Vec<String>) -> &mut AliasOpts {
+        let mut i = 0;
+        let mut pattern_keys: Vec<String> = Vec::new();
+        let mut initialized_keys = false;
+        while i < args.len() {
+            if let Some(mut v) = self.patterns.get(&args[i]) {
+                let mut k = &args[i];
+                if v.0 == CT::Alias {
+                    k = &v.1;
+                    v = self.patterns.get(k).unwrap();
+                }
+                if v.0 == CT::Flag {
+                    self.values.insert(k.to_string(), String::new());
+                } else if i + 1 < args.len() {
+                    // Check if it's moving from one --command to --another.
+                    if !self.patterns.contains_key(&args[i + 1]) {
+                        self.values.insert(k.to_string(), 
+                            args[i + 1].to_string());
+                        i += 1;
+                    }
+                }
+            } else {
+                if !initialized_keys {
+                    for k in self.patterns.keys() {
+                        pattern_keys.push([k, "="].concat().to_string());
+                    }
+                    initialized_keys = true;
+                }
+                let s = args[i].to_string();
+                if let Some(k) = pattern_keys.iter().find(|&k| 
+                    s.starts_with(k)) {
+                    let z = &s[k.len()..];
+                    if z.len() > 0 {
+                        let mut ck = &k[..k.len() - 1];
+                        let mut v = self.patterns.get(ck).unwrap();
+                        if v.0 == CT::Alias {
+                            ck = &v.1;
+                            v = self.patterns.get(&v.1).unwrap();
+                        }
+                        // Taking a --flag would be wrong in the context of
+                        // --flag=something.
+                        if v.0 != CT::Flag {
+                            self.values.insert(ck.to_string(), z.to_string());
+                        }
+                    }
+                } else {
+                    self.loose.push(args[i].to_string());
+                }
+            }
+            i += 1;
+        }
+        self
+    }
+
+    pub fn get(&self, k: &str) -> Option<String> {
+        if let Some(v) = self.values.get(k) {
+            Some(v.to_string())
+        } else {
+            None
+        }
+    }
+
+    pub fn get_loose(&self, i: usize) -> Option<String> {
+        if let Some(v) = self.loose.get(i) {
+            Some(v.to_string())
+        } else {
+            None
+        }
+    }
+
+    pub fn values(&self) -> HashMap<String, String> {
+        self.values.clone()
+    }
+
+    pub fn loose(&self) -> Vec<String> {
+        self.loose.clone()
+    }
+
+    pub fn loose_len(&self) -> usize {
+        self.loose.len()
+    }
+
+}
---
+++ fs/opts/aliasopts/vai/curbs/src/main.rs
@@ -0,0 +1,79 @@
+// main.rs
+// Copyright (c) 2019 Joao Pedrosa
+
+use std::env;
+
+fn lit(a: Vec<&str>) -> Vec<String> {
+    a.iter().map(|s| s.to_string()).collect()
+}
+
+fn sample_url() {
+    let a = lit(vec!["-u", "http://github.com"]);
+    let mut opts = aliasopts::AliasOpts::new();
+    opts.add("--url", aliasopts::CT::CTString)
+        .add_alias("-u", "--url")
+        .parse(a);
+    assert_eq!(opts.get("--url"), Some("http://github.com".to_string()));
+}
+
+fn sample_file() {
+    let a = lit(vec!["-f=shortcut/foo"]);
+    let mut opts = aliasopts::AliasOpts::new();
+    opts.add("--file", aliasopts::CT::CTString)
+        .add_alias("-f", "--file")
+        .parse(a);
+    assert_eq!(opts.get("--file"), Some("shortcut/foo".to_string()));
+}
+
+fn sample_loose() {
+    let a = lit(vec!["foo", "bar", "baz"]);
+    let mut opts = aliasopts::AliasOpts::new();
+    opts.parse(a);
+    assert_eq!(opts.get_loose(2), Some("baz".to_string()));
+}
+
+fn sample_mode() {
+    let a = lit(vec!["--mode", "foo", "bar"]);
+    let mut opts = aliasopts::AliasOpts::new();
+    opts.add("--mode", aliasopts::CT::CTString)
+        .add_alias("-m", "--mode")
+        .parse(a);
+    assert_eq!(opts.get("--mode"), Some("foo".to_string()));
+    assert_eq!(opts.get_loose(0), Some("bar".to_string()));
+}
+
+fn sample_funky_file() {
+    let a = lit(vec!["--file", "foo", "-f", "bar", "--file", "baz"]);
+    let mut opts = aliasopts::AliasOpts::new();
+    opts.add("--file", aliasopts::CT::CTString)
+        .add_alias("-f", "--file")
+        .parse(a);
+    assert_eq!(opts.get("--file"), Some("baz".to_string()));
+}
+
+fn sample_funky_file2() {
+    let a = lit(vec!["--file", "-f", "foo"]);
+    let mut opts = aliasopts::AliasOpts::new();
+    opts.add("--file", aliasopts::CT::CTString)
+        .add_alias("-f", "--file")
+        .parse(a);
+    assert_eq!(opts.get("--file"), Some("foo".to_string()));
+}
+
+
+fn main() {
+    let a: Vec<String> = env::args().collect();
+    println!("{:?}", &a[1..]);
+    sample_url();
+    sample_file();
+    sample_loose();
+    sample_mode();
+    sample_funky_file();
+    sample_funky_file2();
+    let mut opts = aliasopts::AliasOpts::new();
+    opts.add("--file", aliasopts::CT::CTString)
+        .add_alias("-f", "--file")
+        .parse(a);
+    println!("opts: {:?}", opts);
+    println!("--file: {:?}", opts.get("--file"));
+}
