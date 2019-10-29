cc11c86ac2ce067b71125ad7df7af4abd65a34b7
Joao Pedrosa <joaopedrosa@gmail.com>
2019-10-29 12:39:59 -0300

    Removed some unused commented out code.

--- fs/dir/search_files/vai/list_directories/src/main.rs
+++ fs/dir/search_files/vai/list_directories/src/main.rs
@@ -110,36 +110,3 @@ fn main() {
     }
     println!("{}", z);
 }
-
-
-// use std::time::{Instant};
-
-// // fn lister(name: &str, path: &str) {
-// //     println!("name: {}  path: {}", name, path);
-// // }
-
-// // fn tryMuch(afn: fnMut ()) {
-// //     println!("before");
-// //     fnMut();
-// //     println!("after");
-// // }
-
-// fn main() {
-//     let mut total = 0;
-//     // let mut lister = |name: &str, path: &str| {
-//     //     total += 1;
-//     //     println!("name: {}  path: {}", name, path);
-//     // };
-//     let mut lister = |name: &str, path: &str| {
-//         total += 1;
-//         // println!("name: {}  path: {}", name, path);
-//     };
-//     let time = Instant::now();
-//     search_files::list("c:\\construir\\", &mut lister);
-//     let ms = time.elapsed().as_millis();
-//     // search_files::list("c:\\t_\\", &mut lister);
-//     // search_files::list("c:\\t_", lister);
-//     println!("Hello, world!");
-//     println!("total files: {}", total);
-//     println!("Elapsed: {} ms", ms);
-// }
